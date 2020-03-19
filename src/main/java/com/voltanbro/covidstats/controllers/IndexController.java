2dcf5cb07c2adbd9b154d994ecec91c9f0d4359b
Voltan <etc.sharam@gmail.com>
2020-03-19 05:10:17 +0200

    add controllers and tables in home.html

--- src/main/java/com/voltanbro/covidstats/controllers/IndexController.java
+++ src/main/java/com/voltanbro/covidstats/controllers/IndexController.java
@@ -1,12 +1,21 @@
 package com.voltanbro.covidstats.controllers;
 
 
+import com.voltanbro.covidstats.services.CovidDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    public String home() {
+    @Autowired
+    CovidDataService covidDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("locationStats", covidDataService.getAllStats());
         return "home";
     }
 }
--- src/main/java/com/voltanbro/covidstats/services/CovidDataService.java
+++ src/main/java/com/voltanbro/covidstats/services/CovidDataService.java
@@ -53,5 +53,7 @@ public class CovidDataService {
             this.allStats = newStats;
     }
 
-
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
 }
